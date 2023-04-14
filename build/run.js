86d6aead276a0c63b6de6214377ca495c2de257d
ming <huangminghua0615@163.com>
2023-04-14 15:11:33 +0800

    feat: micro-web

--- build/run.js
+++ build/run.js
@@ -1,20 +1,16 @@
-const childProcess = require('child_process')
 const path = require('path')
+const filePath = require('./filePath')
+const runShell = require('./util').runShell
+// 启动项目
+function runChild () {
+  Object.keys(filePath).forEach(item => {
+    const childPath = filePath[item]
 
-const filePath = {
-  vue2: path.join(__dirname, '../vue2-web'),
-  vue3: path.join(__dirname, '../vue3-web'),
-  react: path.join(__dirname, '../react-web'),
-  service: path.join(__dirname, '../service'),
-}
+    runShell(`cd ${childPath} && npm start`)
+  });
 
-function runChild() {
-  Object.values(filePath).forEach(item => {
-    childProcess.spawn(`cd ${item} && npm start`, {
-      stdio: 'inherit',
-      shell: true
-    })
-  })
+  console.log('start success, open browser now');
+  runShell(`node ${path.join(__dirname, './openBrowser.js')}`)
 }
 
 runChild()
--- service/app.js
+++ service/app.js
@@ -1,16 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
-const koaCors = require('koa2-cors')
+const cors = require('koa2-cors')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const react15 = require('./routes/react15')
+const react16 = require('./routes/react16')
 const vue2 = require('./routes/vue2')
 const vue3 = require('./routes/vue3')
-const react = require('./routes/react')
 
 // error handler
 onerror(app)
@@ -21,8 +21,12 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
-app.use(koaCors())
 app.use(require('koa-static')(__dirname + '/public'))
+app.use(cors())
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
 
 // logger
 app.use(async (ctx, next) => {
@@ -33,11 +37,10 @@ app.use(async (ctx, next) => {
 })
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(react15.routes(), react15.allowedMethods())
+app.use(react16.routes(), react16.allowedMethods())
 app.use(vue2.routes(), vue2.allowedMethods())
 app.use(vue3.routes(), vue3.allowedMethods())
-app.use(react.routes(), react.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
